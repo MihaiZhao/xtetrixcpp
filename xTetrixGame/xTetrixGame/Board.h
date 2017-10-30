bedacf2d928285a2922b3ec67e8879f343facb06
MihaiZhao <mihai.zhao@gmail.com>
2017-10-30 11:48:12 +0000

    Made borders as pointer to user input

--- xTetrixGame/xTetrixGame/Board.h
+++ xTetrixGame/xTetrixGame/Board.h
@@ -4,6 +4,7 @@
 
 // ======= Defines =======
 
+const int k_BoardLineWidth = 6;
 #define BOARD_LINE_WIDTH 6			// Width of each of the two lines that delimit the board
 #define BLOCK_SIZE 16				// Width and Height of each block of a piece
 #define BOARD_POSITION 320			// Center position of the board from the left of the screen
@@ -15,13 +16,32 @@
 
 class Board
 {
+private:
+	static int const msMaxRows = 100;		// Static INT - Max numbers of rows
+	static int const msMaxColumns = 100;	// Static INT - Max numbers of columns
+
+	int mMinRows;							// INT - Min numbers of rows
+	int mMinColumns;						// INT - Min numbers of columns
+
+	int input_mRowsFromUser;				// INT - Input from user for rows
+	int input_mColumnsFromUser;				// INT - Input from user for columns
+
+	int mBoardLimitRight;					// INT - Right Board
+	int *pmBoardLimitLeft;					// INT Pointer * - Left Board
+	int *pmBoardLimitBottom;				// INT pointer * - Bottom Board
+
+
+	char mBoard[msMaxRows][msMaxColumns];	// CHAR - Creates the board
+
+
 public:
-	Board();
-	~Board();
+	Board();									// Constructor
+	~Board();									// Destructor
 
 	// Functions
-	void initBoard();
-	void ShowTheBoard();
+	void initBoard();							// Initialize the board
+	void getRowsAndColumnsFromUser();			// Gets the rows and columns from the user input
+	void setTheBoardLimits();					// Sets the board borders
 
 };
 
